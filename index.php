56c845d0030e8689e8d687c4135dfc6bf5608684
Nicola-Perera <nicola.perera96@mail.com>
2021-12-03 17:22:43 +0100

    add different questions to faqs array

--- index.php
+++ index.php
@@ -8,7 +8,21 @@ $faqs = [
     [
         'question' => "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio",
         'answers' => ['risposta 1', 'risposta 2', 'risposta 3']
-    ]
+    ],
+    [
+        'question' => "Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+        'answers' => ['risposta 1', 'risposta 2', 'risposta 3']
+    ],[
+        'question' => "Perché il mio account è associato a un paese?",
+        'answers' => ['risposta 1', 'risposta 2', 'risposta 3']
+    ],[
+        'question' => "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
+        'answers' => ['risposta 1', 'risposta 2', 'risposta 3']
+    ],[
+        'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
+        'answers' => ['risposta 1', 'risposta 2', 'risposta 3']
+    ],
+
 ];
 
 var_dump ($faqs[0]['answers']);
